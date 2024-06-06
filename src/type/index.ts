1d6276f9156b20dd2fb07fa59a6a519726aad86b
Roger BC <roger@chaindots.com>
2024-06-06 10:37:30 -0300

    add: file types

---
+++ src/type/index.ts
@@ -0,0 +1,58 @@
+
+export interface Articles {
+  articles: Article[];
+}
+
+export interface Article {
+  _id:          string;
+  display_date: Date;
+  headlines:    Headlines;
+  promo_items?: PromoItems;
+  subtype:      string;
+  taxonomy:     Taxonomy;
+  website_url:  string;
+}
+
+export interface Headlines {
+  basic: string;
+}
+
+export interface PromoItems {
+  basic: Basic;
+}
+
+export interface Basic {
+  resized_urls?: ResizedURL[];
+  subtitle?:     string;
+  type:          Type;
+  url?:          string;
+}
+
+export interface ResizedURL {
+  option:     Option;
+  resizedUrl: string;
+}
+
+export interface Option {
+  media: Media;
+}
+
+export enum Media {
+  MinWidth20Em = "(min-width: 20em)",
+  MinWidth48Em = "(min-width: 48em)",
+  MinWidth64Em = "(min-width: 64em)",
+}
+
+export enum Type {
+  Image = "image",
+  Video = "video",
+}
+
+export interface Taxonomy {
+  tags: Tag[];
+}
+
+export interface Tag {
+  slug: string;
+  text: string;
+}
